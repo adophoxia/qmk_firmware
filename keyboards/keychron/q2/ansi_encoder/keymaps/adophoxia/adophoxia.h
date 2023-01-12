204fbbfed64bd64dbb18446e79d61a5c16cdf797
Adophoxia <andyao1528@gmail.com>
2023-01-11 21:15:24 -0800

    Add LED indicators; Move functions to separate files; disable Dip Switch

---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/adophoxia.h
@@ -0,0 +1,20 @@
+#pragma once
+
+enum custom_keycodes {
+    KC_WLCK
+};
+
+enum layers{
+    WIN_BASE,
+    WIN_FN,
+    WIN_MM
+};
+
+#define KC_TASK LGUI(KC_TAB)
+#define KC_FLXP LGUI(KC_E)
+#define TSK_EXP C(S(KC_ESC))
+#define KC_WLCK MAGIC_TOGGLE_GUI
+
+/* Use 3 dynamic keymap layers */
+#undef DYNAMIC_KEYMAP_LAYER_COUNT
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
---
+++ keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/indicators.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// un-used, at least on the code provided
+void breathing_win_lock(void);
+void shift_indicators(void);
+void breathing_caps_lock(void);
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max);
