ef8db433d37665fd534bb8a7f57661f2c1d15f77
Adophoxia <andyao1528@gmail.com>
2023-01-22 21:45:49 -0800

    Set `KC_WLCK` to be `SAFE_RANGE`

--- keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/adophoxia.h
+++ keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/adophoxia.h
@@ -1,7 +1,7 @@
 #pragma once
 
 enum custom_keycodes {
-    KC_WLCK,
+    KC_WLCK = SAFE_RANGE,
     ENC_LFT,
     ENC_RGT,
     ENC_PRS
@@ -20,17 +20,3 @@ enum layers{
 #define KC_WLCK MAGIC_TOGGLE_GUI
 #define CTL_GRV MT(MOD_LCTL, KC_GRV)  // Left Control when held, Grave accent when tapped `
 #define ALT_GRV MT(MOD_LALT, KC_GRV)  // Left Alt when held, Grave accent when tapped ~
-
-/* Use 3 dynamic keymap layers */
-#undef DYNAMIC_KEYMAP_LAYER_COUNT
-#define DYNAMIC_KEYMAP_LAYER_COUNT 3
-
-// ENCODER ACTIONS
-#ifdef ENCODER_ENABLE
-    void encoder_action_volume(bool clockwise);
-    void encoder_action_mediatrack(bool clockwise);
-    void encoder_action_navpage(bool clockwise);
-
-    
-
-#endif // ENCODER_ENABLE
