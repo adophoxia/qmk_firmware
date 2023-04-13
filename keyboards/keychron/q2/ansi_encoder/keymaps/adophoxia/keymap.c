640d1eea1a8ddf897109d0dfa5c1f0fb7840b69f
Adophoxia <andyao1528@gmail.com>
2023-04-12 20:45:31 -0700

    Delete

--- keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/keymap.c
+++ keyboards/keychron/q2/ansi_encoder/keymaps/adophoxia/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LWIN, KC_LALT,                            KC_SPC,                             KC_RALT, LAYER1,   LAYER2,   KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_FN] = LAYOUT_ansi_67(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   _______,          ENC_PRS,
+        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_DEL,           ENC_PRS,
         RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______,  _______,  _______,          KC_HOME,
         TSK_EXP, RGB_RMOD,RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,            _______,          KC_END,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______,
