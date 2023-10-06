f02c4e4f862295d66d259b6eff06bdbc68638320
Adophoxia <andyao1528@gmail.com>
2023-10-06 14:59:07 -0700

    Update Keychron Q6

--- keyboards/keychron/q6/iso/config.h
+++ keyboards/keychron/q6/iso/config.h
@@ -25,9 +25,7 @@
 #define NO_PIN_OFFSET 1
 
 /* RGB Matrix Configuration */
-#define DRIVER_1_LED_TOTAL 60
-#define DRIVER_2_LED_TOTAL 49
-#define RGB_MATRIX_LED_COUNT (DRIVER_1_LED_TOTAL + DRIVER_2_LED_TOTAL)
+#define RGB_MATRIX_LED_COUNT 109
 
 /* Enable caps-lock LED*/
 #define CAPS_LOCK_LED_INDEX 60
