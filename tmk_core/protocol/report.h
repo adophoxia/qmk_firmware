f4a165dfe86d68ac3caf615a830b6e05a919a10d
Adophoxia <andyao1528@gmail.com>
2025-02-11 05:48:55 -0800

    Add adaptive NKRO

--- tmk_core/protocol/report.h
+++ tmk_core/protocol/report.h
@@ -44,6 +44,15 @@ enum hid_report_ids {
 
 #define IS_VALID_REPORT_ID(id) ((id) >= REPORT_ID_ALL && (id) <= REPORT_ID_COUNT)
 
+#ifdef APDAPTIVE_NKRO_ENABLE
+/* Keyboard report type */
+#define KB_RPT_MASK(n) (1 << (n))
+enum kb_reports {
+    KB_RPT_STD = KB_RPT_MASK(0),
+    KB_RPT_NKRO = KB_RPT_MASK(1)
+};
+#endif
+
 /* Mouse buttons */
 #define MOUSE_BTN_MASK(n) (1 << (n))
 enum mouse_buttons {
@@ -340,7 +349,7 @@ void add_key_byte(report_keyboard_t* keyboard_report, uint8_t code);
 void del_key_byte(report_keyboard_t* keyboard_report, uint8_t code);
 #ifdef NKRO_ENABLE
 void add_key_bit(report_nkro_t* nkro_report, uint8_t code);
-void del_key_bit(report_nkro_t* nkro_report, uint8_t code);
+bool del_key_bit(report_nkro_t* nkro_report, uint8_t code);
 #endif
 
 void add_key_to_report(uint8_t key);
